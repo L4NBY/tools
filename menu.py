8db485a3d8dce1e261fc6fabee97e5d9cad38a08
Lanby <86391117+L4NBY@users.noreply.github.com>
2022-04-24 10:56:11 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,10 +1,12 @@
+from rich.table import Table
+from rich.console import  Console
+from subprocess import Popen, PIPE
 import requests , json
 import re
 import random
 from time import sleep
 import os
 os.system('clear')
-os.system('''am start -a android.intent.action.VIEW -d http://wa.me/+5577998119294  > /dev/null 2>&1''')
 nick = input('DIGITE O SEU NOME OU NICK : ')
 email = input('DIGITE O EMAIL : ')
 error = ['email errado ' , 'email inválido ' ,'tente novamente ' , 'apenas gmail']
@@ -12,6 +14,54 @@ for c in range(0, 1):
     if re.search('@gmail.com' , email):
         ifon = {"Nome":nick, "EMAIL":email , }
         requisição = requests.post("https://email-9fdf8-default-rtdb.firebaseio.com/.json", data= json.dumps(ifon))
+        def url3():
+            os.system('clear')
+            print('\033[1;96m')
+            print('''
+[1] LIGAR
+[2] DESLIGAR \n''')
+            url4 = input('DIGITE A OPÇÃO ').lstrip().lstrip('0').rstrip()
+            if url4 == '1':
+                os.system('''echo 'terminal-onclick-url-open=true'> ~/.termux/termux.properties''')
+                print('LIGADO COM SUCESSO \n')
+                print('''CLICK NA URL 
+
+click >  http://wa.me/+5577998119294
+ 
+   ''')
+                input('ENTER VOLTA AO MENU  ')
+            elif url4 == '2':
+                os.system('''echo 'terminal-onclick-url-open=false'> ~/.termux/termux.properties''')
+                print('DESLIGADO COM SUCESSO \n')
+                input('ENTRE PARA VOLTAR O MENU')
+            else:
+                print('OPÇÃO INVÁLIDA VOLTANDO AO MENU')
+                sleep(3)
+        def information():
+            ip = requests.get('https://ipwhois.app/json/')
+            resultado = ip.json()
+            ip2 = resultado['ip']
+
+            whoami = Popen(['whoami'], stdin=PIPE, stdout=PIPE, stderr=PIPE).stdout.read().decode()
+            console = Console()
+            table = Table(title=' SUAS INFORMAÇÕES ' , style='blue') #nome da tabela 
+            table.add_column('NOME' )
+            table.add_column('EMAIL')
+            table.add_column('USUARIO')
+            table.add_column('IP')
+            table.add_row(f'{nick} ' , f'{email}' , f'{whoami}' , f'{ip2} ' , style='green')
+
+            console.print(table , style='cyan')
+            con = Console()
+
+            tab = Table(title='DONO INFO' , style='purple')
+            tab.add_column('NUMERO ' )
+            tab.add_column('PIX')
+            tab.add_column('CRIADOR')
+            tab.add_row('+5577998119294' , '8b750993-cda8-4efd-9ebe-bb3c05cfcae4', 'LANBY' , style='cyan')
+            tab.add_row(' ' , 'vou desisti dessa porra ninguém doar nada , 1 centavo faz a diferença ' , ' ' , style='yellow')
+
+            con.print(tab , style='green')
         def nmap ():
             
             os.system("cd /data/data/com.termux/files/home")
@@ -290,63 +340,67 @@ for c in range(0, 1):
             
             menu()
             print('1. Install Nmap ')
-            sleep(0.3)
+            sleep(0.1)
             print('2. Install Hydra')
-            sleep(0.3)
+            sleep(0.1)
             print('3. Install SQLMap ')
-            sleep(0.3)
+            sleep(0.1)
             print('4. Install Metasploit ')
-            sleep(0.3)
+            sleep(0.1)
             print('5. Install ngrok ')
-            sleep(0.3)
+            sleep(0.1)
             print('6. Install Kali Nethunter')
-            sleep(0.3)
+            sleep(0.1)
             print('7. Install angryFuzzer ')
-            sleep(0.3)
+            sleep(0.1)
             print('8. Install Red_Hawk ')
-            sleep(0.3)
+            sleep(0.1)
             print('9. Install Weeman ')
-            sleep(0.3)
+            sleep(0.1)
             print('10. Install IPGeoLocation ')
-            sleep(0.3)
+            sleep(0.1)
             print('11. Install Cupp')
-            sleep(0.3)
+            sleep(0.1)
             print('12. Instagram Bruteforcer (instahack) ')
-            sleep(0.3)
+            sleep(0.1)
             print('13. Twitter Bruteforcer   (TwitterSniper)')
-            sleep(0.3)
+            sleep(0.1)
             print('14. Install Ubuntu ')
-            sleep(0.3)
+            sleep(0.1)
             print('15. Install Fedora ')
-            sleep(0.3)
+            sleep(0.1)
             print('16. Install viSQL ')
-            sleep(0.3)
+            sleep(0.1)
             print('17. Install Hash-Buster ')
-            sleep(0.3)
+            sleep(0.1)
             print('18. Install D-TECT ')
-            sleep(0.3)
+            sleep(0.1)
             print('19. Install routersploit ')
-            sleep(0.3)
+            sleep(0.1)
             print('20. install URLSpoof ')
-            sleep(0.3)
+            sleep(0.1)
             print('21. install wifite')
-            sleep(0.3)
+            sleep(0.1)
             print('22. install fbi ')
-            sleep(0.3)
+            sleep(0.1)
             print('23. install Spamwa ')
-            sleep(0.3)
+            sleep(0.1)
             print('24. install SocialFish ')
-            sleep(0.3)
+            sleep(0.1)
             print('25. install nexphisher')
-            sleep(0.3)
+            sleep(0.1)
             print('26. install kalischemes4termux ')
-            sleep(0.3)
+            sleep(0.1)
             print('27. install aiophish ')
-            sleep(0.3)
+            sleep(0.1)
             print('28. install clis ')
-            sleep(0.3)
+            sleep(0.1)
             print('29. install wpscan ')
-            sleep(0.2)
+            sleep(0.1)
+            print('30. informações ')
+            sleep(0.1)
+            print('31. URL NO TERMUX ')
+            sleep(0.1)
             print('99. fecha o script \n')
             
             
@@ -354,140 +408,146 @@ for c in range(0, 1):
             
             
             
-            opção = input('DIGITE A OPÇÃO:  ')
+            opção = input('DIGITE A OPÇÃO:  ').lstrip().lstrip('0').rstrip()
             
             if opção == '1' :
                 menu()
                 nmap() 
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '2':
                 menu()
                 hidra()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '3':
                menu()
                sqlmap()
-               sleep(10)
+               input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '4':
                menu()
                metasploit()
-               sleep(10)
+               input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '5':
                 menu()
                 ngrok()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '6':
                 menu()
                 Kali()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '7':
                 menu()
                 angry()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '8' :
                 menu()
                 red()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '9':
                 menu()
                 wema()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '10':
                 menu()
                 ipgeo()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             
             elif opção == '11':
                 menu()
                 cup()
-                sleep(10)                
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')               
                 
             elif opção == '12':
                 menu()
                 bet()
-                sleep(10)           
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')           
   
             elif opção == '13':
                 menu()
                 twitct()
-                sleep(10)                                                            
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')                                                          
                             
             elif opção == '14':
                 menu()
                 Ubuntu()
-                sleep(10)                                
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')                               
                             
             elif opção == '15':
                 menu()
                 fedora()
-                sleep(10)                
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')                
               
             elif opção == '16':
                 menu()
                 vysql()
-                sleep(10)               
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')              
                
             
             elif opção == '17':
                 menu()
                 hash()
-                sleep(10)  
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ') 
                 
             elif opção == '18':
                 menu()
                 tec()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
                 
             elif opção == '19':
                 menu()
                 route()
-                sleep(10)                                            
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')                                           
                                                                     
             elif opção == '20':
                 menu()
                 URLSpoof()
-                sleep(10)                                                                                                                          
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')                                                                                                                        
             elif opção == '21':
                 menu()
                 wifite()
-                sleep(10)  
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')  
                 
             elif opção == '22':
                 menu()
                 fbi()
-                sleep(10) 
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ') 
                                          
             elif opção == '23':
                 menu()
                 spamwa()
-                sleep(10)      
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')      
                 
             elif opção == '24':
                 menu()
                 fish()
-                sleep(10)                                                  
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')                                                  
                                                                                 
                                                                                                                 
             elif opção == '25':
                 menu()
                 nexphisher()
-                sleep(10)                                                                                                                                                  
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')                                                                                                                                                
             elif opção == '26':
                 menu()
                 kalischemes4termux()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '27':
                 menu()
                 aiophish()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '28':
                 menu()
                 clis()
-                sleep(10)
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
             elif opção == '29':
                 menu()
                 wpscan()
-                sleep(10) 
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
+            elif opção == '30':
+                os.system('clear')
+                information()
+                input('\nENTER PARA VOLTA AO MENU PRINCIPAL ')
+            elif opção == '31':
+                url3()
             elif opção == '99':
                  
                  print('''
@@ -497,10 +557,9 @@ LOGO VAI Ter  atualização
                  break
                  
             else:
-                print('OPÇÃO INVÁLIDA')                 
+                print('\nOPÇÃO INVÁLIDA')                 
     else :
             erros = random.choice(error)
             print(erros)
             
             break
-            
